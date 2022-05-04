3ceab55d87779e84a4a65abb71aef385d113a382
KozhaAkhmet <80167990+KozhaAkhmet@users.noreply.github.com>
2022-05-04 12:58:17 +0300

    Main Code

---
+++ Saat_ile_ornek/Zaman1.h
@@ -0,0 +1,22 @@
+//
+// Created by sk8 on 01.05.2022.
+//
+
+#ifndef UNTITLED3_ZAMAN1_H
+#define UNTITLED3_ZAMAN1_H
+
+class Zaman{
+public:
+    Zaman();
+    void zamanAyarla (int, int, int);
+    void askeriYaz ();
+    void standartYaz ();
+
+private:
+    int saat;
+    int dakika;
+    int saniye;
+};
+
+
+#endif //UNTITLED3_ZAMAN1_H
---
+++ Saat_ile_ornek/cmake-build-debug/CMakeFiles/untitled3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled3.dir/Calculate_volume_of_sphere.cpp.o"
+  "CMakeFiles/untitled3.dir/Calculate_volume_of_sphere.cpp.o.d"
+  "untitled3"
+  "untitled3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
